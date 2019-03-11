c2a4f2f3a27c31f75812636fcc58ee98f840c18a
Mawande Hobo <G17H5759@ict.ru.ac.za>
2019-03-12 00:44:16 +0200

    Solved

--- Synthesis/Library.fs
+++ Synthesis/Library.fs
@@ -59,7 +59,14 @@ let minmax (a,b,c,d )=
    
 
 let isLeap year =
- failwith "invalid year"
+    match year >=1582 with
+    |true-> match (year%4=0 && year%100<>0) with
+            |true-> true
+            |_->year%400=0
+    
+    |_->failwith "Invalid"
+
+ 
  
    
 
@@ -79,17 +86,67 @@ let month  =function
                 |12->"December", 31
                 |_-> failwith "Not implemented"
   
+
+
 let toBinary num =
-    
-    failwith "Negative value"
+    let rec biNary digit str=
+        match digit<0 with
+        |true->failwith "Negative digit"
+        |_->match digit=0 && str<>"" with
+            |true->str
+            |false-> match digit%2<>0 with
+                     |true->biNary (digit/2) ("1"+str)
+                     |_->biNary (digit/2) ("0"+str)
+    biNary num ""
+   
 
   
 
-let bizFuzz _ =
-    failwith "Not implemented"
+let bizFuzz num=
+    let rec Count take (count, count1 ,count2)=
+        match take>=1 with
+         |true -> match (take%3=0,take%5=0)  with
+                  |true,true -> Count (take-1) ((count+1),(count1+1),(count2+1))
+                  |true,false->Count (take-1) ((count+1),(count1+0),(count2+0))
+                  |false,true->Count (take-1) (count+0,count1+1,count2+0)
+                  |_ -> Count (take-1) ((count+0),(count1+0),(count2+0))          
+         |_-> (count,count1,count2)
+    Count num (0, 0, 0)
+   
 
-let monthDay _ _ =
-    failwith "Not implemented"
 
-let coord _ =
-    failwith "Not implemented"
+
+
+
+
+
+let monthDay day year =
+    match isLeap year with
+    |false -> match  (day>=1 && day<=365) with
+         |true-> match day%12 with
+                 |1->"January"
+                 |2->"February"
+                 |3->"March"
+                 |4->"April"
+                 |5->"May"
+                 |6->"June"
+                 |7->"July"
+                 |8->"August"
+                 |9->"September"
+                 |10->"October"
+                 |11->"November"
+                 |12->"December"
+                 
+
+         |false->failwith "llb"
+    |_->failwith "Invalid date"
+
+
+
+   
+
+let coord (a,b) (c,d) =
+ sqrt (((a-c)*(a-c)) +((b-d)*(b-d)))  
+
+
+   
